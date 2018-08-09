fb20f491d7a778fd847c283d8f88356148ec00e0
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-08-08 20:14:55 -1000

    Debug

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const net = require("net");
 const server = net.createServer(client => {
   console.log("CLIENT CONNECTED!");
   client.write(client.name, client.address());
+  client.on("data", data => {
+    console.log(data);
+  });
 });
 
 server.listen(6969, () => {
-  console.log("Server listening on port 2020");
+  console.log("Server listening on port 6969");
 });
